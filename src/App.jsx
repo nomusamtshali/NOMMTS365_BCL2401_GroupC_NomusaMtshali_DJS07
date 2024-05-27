3081499655e2982d054c941206cd52c30907d84e
Nomusa Mtshali <kelebogilemataboge17@gmail.com git config --global user.email kelebogilemataboge17@gmail.com>
2024-05-27 21:05:23 +0200

    created a header component

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+
+export default function App() {
+
+  return (
+    <>
+    <div>
+      <Header />
+    </div>
+      </>
+  );
+};
+
+
